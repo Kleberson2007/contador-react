59041c1b92db6cbfe5b22062fa6e29a083d0cf25
Kleberson dos Santos Mendes Pinto <klebersonsantosmp@gmail.com>
2024-06-26 16:47:24 -0300

    :)

--- src/App.js
+++ src/App.js
@@ -4,7 +4,21 @@ import React, { useState } from 'react';
 export default function App() {
     console.log("renderizando App()...")
     let [numero, setNumero] = useState(0)
+    const limiteMinimo = -100000;
+    const limiteMaximo = 100000;
 
+        if (numero < limiteMinimo) {
+            numero = limiteMinimo;
+        } 
+        else
+        if (numero > limiteMaximo) {
+            numero = limiteMaximo;
+        }
+
+    function numeroaleatorio() {
+        const numeroaleatorio = Math.floor(Math.random() * 200000) - 100000;
+        setNumero(numeroaleatorio)
+    }
         function menos10000() {
             setNumero(numero - 10000)
             console.log(numero)
@@ -62,20 +76,21 @@ export default function App() {
     return (
         <section>
             <h1>Contador</h1>
+            <button className='aleatorio' onClick={numeroaleatorio}>aleatório 1-100.000</button>
             <p>{numero}</p>
-            <div class="botoes">
-            <button onClick={menos10000}>-10000</button>
-            <button onClick={menos1000}>-1000</button>
-            <button onClick={menos100}>-100</button>
-            <button onClick={menos10}>-10</button>
-            <button onClick={menos1}>-1</button>
-            <button onClick={mais1}>+1</button>
-            <button onClick={mais10}>+10</button>
-            <button onClick={mais100}>+100</button>
-            <button onClick={mais1000}>+1000</button>
-            <button onClick={mais10000}>+10000</button>
+            <div className="botoes">
+            <button className='negativos' onClick={menos10000}>-10000</button>
+            <button className='negativos' onClick={menos1000}>-1000</button>
+            <button className='negativos' onClick={menos100}>-100</button>
+            <button className='negativos' onClick={menos10}>-10</button>
+            <button className='negativos' onClick={menos1}>-1</button>
+            <button className='positivos' onClick={mais1}>+1</button>
+            <button className='positivos' onClick={mais10}>+10</button>
+            <button className='positivos' onClick={mais100}>+100</button>
+            <button className='positivos' onClick={mais1000}>+1000</button>
+            <button className='positivos' onClick={mais10000}>+10000</button>
             </div>
-            <button onClick={zerar}>zerar</button>
+            <button className='zerar' onClick={zerar}>zerar</button>
         </section>
     )
 }
